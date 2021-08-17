50b6fbfef00b599bdc5b64e431a5f564b44fc0db
diyanamendoza <diyana.amp@pm.me>
2021-08-17 14:06:05 -0700

    Add initial JS

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 // import functions and grab DOM elements
+import { compareNumbers } from './utils.js';
+const guessedNumber = document.getElementById('number-input');
+const guessButton = document.getElementById('guess-button');
+const displayedResult = document.getElementById('result-space');
 
 // initialize global state
+let guessCount = 0;
 
+let correctAnswer = Math.ceil(Math.random() * 20);
 // set event listeners 
+guessButton.addEventListener('click', () => {
+  let currentGuess = (Number(guessedNumber.value));
+  let result = compareNumbers(currentGuess, correctAnswer);
+
+  if (result === 1) {
+    displayedResult.textContent = 'Too high! Guess again.' }
+    else if (result === 0) {
+    displayedResult.textContent = 'Holy wow, you got it!' }
+    else {
+    displayedResult.textContent = 'Too low! Guess again.'}
+
+  guessCount++;
+  console.log(result);
+  console.log(correctAnswer);
+  console.log(guessCount);
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+ 
