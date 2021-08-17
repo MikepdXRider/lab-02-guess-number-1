5e92b021f255f4112b4c643e797229fe8a481c57
diyanamendoza <diyana.amp@pm.me>
2021-08-17 13:38:42 -0700

    Add tests for compareNumbers

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,51 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+// // IMPORT MODULES under test here:
+// import { compareNumbers } from '../utils.js';
 
-const test = QUnit.test;
+// const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+// test('when guess and correctNumber are the same, should output 0', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const guess1 = 1;
+//     const correct1 = 1;
+//     const expected = 0;
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = compareNumbers(guess1, correct1);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
-});
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
+
+// test('when guess is less than correctNumber, should output -1', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const guess2 = 1;
+//     const correct2 = 3;
+//     const expected = -1;
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = compareNumbers(guess1, correct1);
+
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
+
+
+// test('when guess is greater than correctNumber, should output 1', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const guess3 = 4;
+//     const correct3 = 2;
+//     const expected = 1;
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = compareNumbers(guess1, correct1);
+
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
+
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,50 @@
-import './example.test.js';
+import { compareNumbers } from '../utils.js';
+
+const test = QUnit.test;
+
+test('when guess and correctNumber are the same, should output 0', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const guess1 = 1;
+    const correct1 = 1;
+    const expected = 0;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(guess1, correct1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('when guess is less than correctNumber, should output -1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const guess2 = 1;
+    const correct2 = 3;
+    const expected = -1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(guess2, correct2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('when guess is greater than correctNumber, should output 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const guess3 = 4;
+    const correct3 = 2;
+    const expected = 1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(guess3, correct3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
